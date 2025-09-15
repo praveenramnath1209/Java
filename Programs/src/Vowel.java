31efa02ed50d4c7982ea63914824a05c07334f99
Praveen Ramnath J <Praveen_Ramnath12@outlook.com>
2025-09-15 20:16:49 +0530

    files

--- Programs/src/Vowel.java
+++ Programs/src/Vowel.java
@@ -3,5 +3,10 @@ import java.util.*;
 class Vowel{
 	public static void main(String [] args){
 		Scanner y = new Scanner(System.in);
+		char c = y.next().charAt(0);
+		c = Character.toLowerCase(c);
+		char[] cx = {'a','e','i','o','u'};
+		String result = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? "Vowel" : "Consonant";
+        System.out.println(result);		
 	}
 }
